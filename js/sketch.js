6e561d46aa49de93f7bf43bffca1683315f30a97
Akiva Thalheim <akiva@joinsaturn.com>
2020-12-08 16:09:41 -0800

    click to fly

--- js/sketch.js
+++ js/sketch.js
@@ -4,6 +4,10 @@ var world;
 // create a variable to hold our marker
 var marker;
 
+var plane
+var elevation = 1
+var gravity = 0.01
+
 function setup() {
   // create our world (this also creates a p5 canvas for us)
   world = new World('ARScene');
@@ -11,21 +15,25 @@ function setup() {
   // grab a reference to the marker that we set up on the HTML side (connect to it using its 'id')
   marker = world.getMarker('hiro');
 
-  enemyPlane = new OBJ({
+  plane = new OBJ({
 		asset: 'plane_obj',
     mtl: 'plane_mtl',
-		x: 0, y:0, z:0,
+		x: 0, y:elevation, z:0,
     rotationY: -90,
     scaleX: 0.15,
 		scaleY: 0.15,
 		scaleZ: 0.15
 	});
-	marker.addChild(enemyPlane);
+	marker.addChild(plane);
 }
 
 
 function draw() {
 
+    elevation -= gravity
+    plane.setY(elevation)
 
-
+    if (mouseIsPressed == true){
+      elevation += .05
+    }
 }
